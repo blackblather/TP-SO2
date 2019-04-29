de1a1cbe85dd06000aecfd4ed318e45f4cd431bb
Joao Monteiro <joao.monteiro95@hotmail.com>
2019-04-29 21:26:03 +0100

    Created Resources.h and some base structs

---
+++ Server/Resources.h
@@ -0,0 +1,59 @@
+#pragma once
+//Server Constants------------------------
+#define CMD_SIZE 128
+//COORDINATES
+struct coordinates_STRUCT {
+	int x,
+		y;
+} typedef _coordinates;
+//GAME AREA-------------------------------
+struct gameArea_STRUCT {
+	int width,
+		height;
+} typedef _gameArea;
+
+//BALL------------------------------------
+enum ballDirection_ENUM {
+	topRight,
+	topLeft,
+	bottomRight,
+	bottomLeft
+} typedef _ballDirection;
+struct ball_STRUCT {
+	_ballDirection direction;
+	_coordinates coordinates;
+	int speed;
+} typedef _ball;
+
+//BASE------------------------------------
+struct base_STRUCT {
+	_coordinates coordinates;
+	int size,
+		speed;
+} typedef _base;
+
+//BLOCK-----------------------------------
+enum blockType_ENUM {
+	normal,
+	strong,
+	magic
+} typedef _blockType;
+struct block_STRUCT {
+	_blockType type;
+	_coordinates coordinates;
+	int size;
+} typedef _block;
+
+//PERK------------------------------------
+enum perkType_ENUM {
+	speedUp,
+	slowDown,
+	extraLife,
+	triple
+} typedef _perkType;
+struct perk_STRUCT {
+	_perkType type;
+	_coordinates coordinates;
+	int size,
+		speed;
+} typedef _perk;
