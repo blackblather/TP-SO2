ef1f3f0106db1d9637768939f556fa0ac3d1201f
Joao Monteiro <joao.monteiro95@hotmail.com>
2019-04-30 23:17:31 +0100

    Added file views and created simple 'command loop' function to accept user input

--- Server/Source.c
+++ Server/Source.c
@@ -2,78 +2,104 @@
 #include <tchar.h>
 #include <Windows.h>
 #include "Resources.h"
-
+ 
+//Game vars
 _ball ball;
 _gameArea gameArea;
+_gameSettings gameSettings;
+//File mapping vars
+HANDLE hFile;
+HANDLE hFileMapping;
+LPVOID dataFileView, messageFileView;
 
-void InitGameSettings(int gameAreaWidth, int gameAreaHeight) {
-	//Game Area
-	gameArea.width = gameAreaWidth;				// [0, gameAreaWidth[
-	gameArea.height = gameAreaHeight;			// [0, gameAreaHeight[
-	//Ball
-	ball.direction = topRight;
-	ball.coordinates.x = (gameArea.width/2)-1;	//horizontal-center
-	ball.coordinates.y = gameArea.height-1;		//vertical-bottom
 
+//File Mapping
+LPVOID LoadFileView(int offset, int size) {
+	return MapViewOfFile(
+		hFileMapping,
+		FILE_MAP_READ | FILE_MAP_WRITE,
+		0,
+		offset,
+		size);
 }
-
-void LoadMessageFileView(HANDLE hMessageFV) {
-
-}
-
-void LoadDataFileView(HANDLE hDataFV) {
-
-}
-
-BOOL LoadSharedInfo() {
-	//CLOSE HANDLES IN THE END
-	HANDLE hFile = CreateFile(_T("../Server/SharedInfo.txt"),	//File name
-		GENERIC_READ | GENERIC_WRITE,							//Access
-		FILE_SHARE_READ | FILE_SHARE_WRITE,						//Share mode
-		NULL,													//Security attributes
-		OPEN_EXISTING,											//Creation disposition
-		FILE_ATTRIBUTE_NORMAL,									//Flags and attributes
-		NULL);													//Template File (not used)
+BOOL LoadSharedInfo(LPCSTR fileName) {
+	//SRC #1: https://docs.microsoft.com/pt-pt/windows/desktop/api/fileapi/nf-fileapi-createfilea
+	hFile = CreateFile(_T(fileName),								//File name
+		GENERIC_READ | GENERIC_WRITE,								//Access
+		FILE_SHARE_READ | FILE_SHARE_WRITE,							//Share mode
+		NULL,														//Security attributes
+		OPEN_EXISTING,												//Creation disposition
+		FILE_ATTRIBUTE_NORMAL,										//Flags and attributes
+		NULL);														//Template File (not used)
 	if (hFile != INVALID_HANDLE_VALUE) {
 		_tprintf(_T("Successfully opened file\n"));
-		//NOTE #1: "An attempt to map a file with a length of 0 (zero) fails with an error code of ERROR_FILE_INVALID. Applications should test for files with a length of 0 (zero) and reject those files."
-		//SRC #1: https://docs.microsoft.com/en-us/windows/desktop/api/WinBase/nf-winbase-createfilemappinga
-		//NOTE #2: I chose 4096 bytes for the size, because it's the cluster size, 4096 bytes is enough, and the way I'm not uselessly wasting disk space, later on when saving the file.
-		//SRC #2: https://superuser.com/questions/66825/what-is-the-difference-between-size-and-size-on-disk
-		HANDLE hFileMapping = CreateFileMapping(hFile,
+		//SRC #2: https://docs.microsoft.com/en-us/windows/desktop/api/WinBase/nf-winbase-createfilemappinga
+		hFileMapping = CreateFileMapping(hFile,
 			NULL,
 			PAGE_READWRITE,
-			0,													//Size in bytes	(high-order)
-			4096,												//Size in bytes (low-order)
+			0,														//Size in bytes	(high-order)
+			65536*2,												//Size in bytes (low-order)
 			_T("LocalSharedInfo"));
 		
 		if (hFileMapping != NULL) {
 			_tprintf(_T("Successfully mapped file in memory\n"));
-			return TRUE;
-		}
-		else {
+			if ((dataFileView = LoadFileView(0, 65536)) != NULL && (messageFileView = LoadFileView(65536, 65536)) != NULL) {
+				_tprintf(_T("Successfully created file views\n"));
+				return TRUE;
+			} else {
+				DWORD errorCode = GetLastError();
+				_tprintf(_T("%x"), errorCode);
+			}
+		} else {
 			_tprintf(_T("ERROR MAPPING FILE. Exiting...\n"));
 			DWORD errorCode = GetLastError();
 			_tprintf(_T("%x"), errorCode);
-			_tprintf(_T("%x"), errorCode);
 		}
-	}
-	else
+	} else
 		_tprintf(_T("ERROR OPENING FILE. Exiting...\n"));
-
 	return FALSE;
-	//LoadDataFileView();
-	//LoadMessageFileView();
+}
+void CloseSharedInfoHandles() {
+	UnmapViewOfFile(messageFileView);
+	UnmapViewOfFile(dataFileView);
+	CloseHandle(hFileMapping);
+	CloseHandle(hFile);
 }
 
+//Game
+void InitGameSettings(int gameAreaWidth, int gameAreaHeight) {
+	//Game Area
+	gameArea.width = gameAreaWidth;				// [0, gameAreaWidth[
+	gameArea.height = gameAreaHeight;			// [0, gameAreaHeight[
+												//Ball
+	ball.direction = topRight;
+	ball.coordinates.x = (gameArea.width / 2) - 1;	//horizontal-center
+	ball.coordinates.y = gameArea.height - 1;		//vertical-bottom
+
+}
+void Help() {
+	_tprintf(_T("Available commands: \n"));
+	_tprintf(_T("exit -> Orderly closes the server.\n"));
+}
+void CmdLoop() {
+	_TCHAR cmd[CMD_SIZE];
+	_tprintf(_T("-------------------------------------------\n"));
+	_tprintf(_T("Type \"help\" to list available commands.\n"));
+	do {
+		_tprintf(_T("-------------------------------------------\n"));
+		_tprintf(_T("Command: "));
+		_tscanf_s(_T("%s"), cmd, CMD_SIZE - 1);
+		if (_tcscmp(cmd, _T("help")) == 0)
+			Help();
+	} while (_tcscmp(cmd, _T("exit")) != 0);
+}
+
+//Main
 int _tmain(int argc, const _TCHAR* argv[]) {
-	_tprintf(_T("Executable location: %s\n"), argv[0]);
-	if (LoadSharedInfo()) {
-		_TCHAR cmd[CMD_SIZE];
-		while (_tcscmp(cmd, _T("exit"))) {
-			_tprintf(_T("Command: "));
-			_tscanf_s(_T("%s"), cmd, CMD_SIZE - 1);
-		}
+	_tprintf(_T("Arkanoid server:\nExecutable location: %s\n-------------------------------------------\nLoading...\n"), argv[0]);
+	if (LoadSharedInfo(_T("../Server/SharedInfo.txt"))) {
+		CmdLoop();
+		CloseSharedInfoHandles();
 	}
 	return 0;
 }
