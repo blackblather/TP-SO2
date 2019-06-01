3089ca7a2ae865ec7c3434e993bca165558d6742
Joao Monteiro <joao.monteiro95@hotmail.com>
2019-06-01 19:24:57 +0100

    Added "Static" window (it's just a fricking label lol) and changed it's BG color to match the parent window

--- Client/Source.c
+++ Client/Source.c
@@ -4,8 +4,10 @@
 #include "dll.h"
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+static HBRUSH hbrBkgnd = NULL;
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow) {
+	//Source: https://docs.microsoft.com/en-us/windows/desktop/learnwin32/your-first-windows-program
 	// Register the window class.
 	const TCHAR CLASS_NAME[] = TEXT("Sample Window Class");
 
@@ -56,21 +58,48 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
 }
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
 	switch (uMsg) {
-		case WM_CREATE: {	//MSDN: https://docs.microsoft.com/pt-pt/windows/desktop/winmsg/wm-create
-			HWND textBox = CreateWindowEx(0,
-				TEXT("EDIT"),
+		case WM_CREATE: {
+			//Create label
+			CreateWindow(TEXT("Static"),
+				TEXT("Enter username"),
+				WS_CHILD | WS_VISIBLE,
+				50,
+				20,
+				150,
+				25,
+				hwnd,
+				NULL,
+				NULL,
+				NULL);
+
+			//Create textbox
+			HWND usernameTxt = CreateWindow(TEXT("Edit"),
 				TEXT(""),
 				WS_BORDER | WS_CHILD | WS_VISIBLE,
 				50,
-				60,
+				50,
 				200,
 				25,
 				hwnd,
 				NULL,
 				NULL,
 				NULL);
+
 			return 0;
 		} break;
+		case WM_CTLCOLORSTATIC: {
+			//StackOverflow: https://stackoverflow.com/questions/4495509/static-control-background-color-with-c
+			//MSDN: https://docs.microsoft.com/en-us/windows/desktop/controls/wm-ctlcolorstatic
+			HDC hdcStatic = (HDC)wParam;
+			SetTextColor(hdcStatic, RGB(0, 0, 0));
+			SetBkColor(hdcStatic, RGB(255, 255, 255));
+
+			if (hbrBkgnd == NULL)
+			{
+				hbrBkgnd = CreateSolidBrush(RGB(255, 255, 255));
+			}
+			return (INT_PTR)hbrBkgnd;
+		}
 		case WM_CLOSE: {
 			if (MessageBox(hwnd, TEXT("Really quit?"), TEXT("My application"), MB_OKCANCEL) == IDOK)
 				DestroyWindow(hwnd);
