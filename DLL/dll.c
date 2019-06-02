96b05c23483c1881a291d47773db4887d3c2fb50
Joao Monteiro <joao.monteiro95@hotmail.com>
2019-06-02 23:54:40 +0100

    Added functions to open server mapped file and to create both views. Also the function that opens server mapped file, only returns true in case the server has mapped the file firstly

--- DLL/dll.c
+++ DLL/dll.c
@@ -1,5 +1,53 @@
 #include "dll.h"
 
-int InitializeSwag() {
-	return 5;
+//"private" vars
+HANDLE hFileMapping = NULL;
+_gameData* gameDataStart = NULL;
+LPVOID messageStart = NULL;
+SYSTEM_INFO sysInfo;
+
+//"private" functions
+BOOL OpenExistingGameMappedFile() {
+	if (hFileMapping == NULL) {
+		GetSystemInfo(&sysInfo);
+		hFileMapping = CreateFileMapping(INVALID_HANDLE_VALUE,
+			NULL,
+			PAGE_READWRITE,
+			0,										//Size in bytes	(high-order)
+			sysInfo.dwAllocationGranularity * 2,	//Size in bytes (low-order)
+			_T("LocalSharedInfo"));
+		if (hFileMapping != NULL) {
+			if (GetLastError() == ERROR_ALREADY_EXISTS)
+				return TRUE;	//Return handle to file mapped by the server
+			//If the code reaches here, that means the file was NOT mapped in memory
+			//(a.k.a. the server's not running), so close the handle and set hFileMapping to NULL
+			CloseHandle(hFileMapping);
+			hFileMapping = NULL;
+		}
+		return FALSE;
+	}
+	return TRUE;
+}
+LPVOID LoadFileView(int offset, int size) {
+	return MapViewOfFile(
+		hFileMapping,
+		FILE_MAP_READ | FILE_MAP_WRITE,
+		0,
+		offset,
+		size);
+} 
+
+//"public" functions
+BOOL LoadGameFileViews() {
+	return (OpenExistingGameMappedFile() &&
+		(gameDataStart = (_gameData*)LoadFileView(0, sysInfo.dwAllocationGranularity)) != NULL &&
+		(messageStart = LoadFileView(sysInfo.dwAllocationGranularity, sysInfo.dwAllocationGranularity)) != NULL);
+}
+
+void SendUsername(char username[256]) {
+
+}
+
+BOOL LoggedIn(char username[256]) {
+	SendUsername(username);
 }
--- DLL/dll.h
+++ DLL/dll.h
@@ -1,8 +1,16 @@
 #pragma once
+#include <windows.h>
+#include <tchar.h>
+//$(SolutionDir)Server
+#include "C:\\Users\\joaom\\Google Drive\\Licenciatura (2016-xxxx)\\2018 - 2019\\Sem2\\SO2\\TP\\Server\\Resources.h"
+
 #ifdef _WINDLL
 	#define DLL_API __declspec(dllexport)
 #else
 	#define DLL_API __declspec(dllimport)
 #endif
 
-DLL_API int InitializeSwag();
+//Functions
+DLL_API BOOL LoadGameFileViews();
+DLL_API void SendUsername(char username[256]);
+DLL_API BOOL LoggedIn(char username[256]);
