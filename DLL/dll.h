c8bb28a72175571ef629d865193de957ee115928
Joao Monteiro <joao.monteiro95@hotmail.com>
2019-06-03 19:02:44 +0100

    Resources.h is now included in project properties (Had to change the clients properties aswel)

--- DLL/dll.h
+++ DLL/dll.h
@@ -1,8 +1,8 @@
 #pragma once
 #include <windows.h>
 #include <tchar.h>
-//$(SolutionDir)Server
-#include "C:\\Users\\joaom\\Google Drive\\Licenciatura (2016-xxxx)\\2018 - 2019\\Sem2\\SO2\\TP\\Server\\Resources.h"
+#include "Resources.h"
+
 
 #ifdef _WINDLL
 	#define DLL_API __declspec(dllexport)
