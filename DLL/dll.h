de85d28622256b2a8f435bce1948ecdc093b83a0
Joao Monteiro <joao.monteiro95@hotmail.com>
2019-06-06 20:32:07 +0100

    Fixed function name

--- DLL/dll.h
+++ DLL/dll.h
@@ -14,4 +14,4 @@
 DLL_API VOID CloseSharedInfoHandles();
 DLL_API BOOL LoadGameResources();
 DLL_API BOOL LoggedIn(TCHAR username[USERNAME_MAX_LENGHT]);
-DLL_API void WriteClientMsg(WPARAM wParam);
+DLL_API void WritePlayerMsg(WPARAM wParam);
